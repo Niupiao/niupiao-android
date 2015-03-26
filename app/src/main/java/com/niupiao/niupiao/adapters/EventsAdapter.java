7b22972c334536ae40e75e2c332723c651183b97
Ryan Kwon <rynkwn@gmail.com>
2015-03-26 04:16:31 -0400

    EVENTS: Fontwork done.

--- app/src/main/java/com/niupiao/niupiao/adapters/EventsAdapter.java
+++ app/src/main/java/com/niupiao/niupiao/adapters/EventsAdapter.java
@@ -1,6 +1,7 @@
 package com.niupiao.niupiao.adapters;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,8 @@ import com.niupiao.niupiao.R;
 import com.niupiao.niupiao.models.Event;
 import com.niupiao.niupiao.utils.DateUtils;
 import com.niupiao.niupiao.utils.ImageLoaderHelper;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by kmchen1 on 2/21/15.
@@ -37,13 +40,30 @@ public class EventsAdapter extends ParcelableArrayAdapter<Event> {
         ViewHolder viewHolder;
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(resource, parent, false);
+
+            Typeface black = Typeface.createFromAsset(getMainActivity().getAssets(), "fonts/Roboto-Black.ttf");
+            Typeface medium = Typeface.createFromAsset(getMainActivity().getAssets(), "fonts/Roboto-Medium.ttf");
+            Typeface regular = Typeface.createFromAsset(getMainActivity().getAssets(), "fonts/Roboto-Regular.ttf");
+
+            TextView day = (TextView) convertView.findViewById(R.id.tv_day);
+            TextView month = (TextView) convertView.findViewById(R.id.tv_month);
+            TextView name = (TextView) convertView.findViewById(R.id.tv_event_name);
+            TextView time = (TextView) convertView.findViewById(R.id.tv_event_time);
+            TextView location = (TextView) convertView.findViewById(R.id.tv_event_location);
+
+            day.setTypeface(black);
+            month.setTypeface(medium);
+            name.setTypeface(medium);
+            time.setTypeface(medium);
+            location.setTypeface(regular);
+
             viewHolder = new ViewHolder();
             viewHolder.image = (NetworkImageView) convertView.findViewById(R.id.image);
-            viewHolder.day = (TextView) convertView.findViewById(R.id.tv_day);
-            viewHolder.month = (TextView) convertView.findViewById(R.id.tv_month);
-            viewHolder.name = (TextView) convertView.findViewById(R.id.tv_event_name);
-            viewHolder.time = (TextView) convertView.findViewById(R.id.tv_event_time);
-            viewHolder.location = (TextView) convertView.findViewById(R.id.tv_event_location);
+            viewHolder.day = day;
+            viewHolder.month = month;
+            viewHolder.name = name;
+            viewHolder.time = time;
+            viewHolder.location = location;
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();

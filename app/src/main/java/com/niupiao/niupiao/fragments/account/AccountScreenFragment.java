3856eaf13e67d3c808408429dd5f73eb820ff772
Ryan Kwon <rynkwn@gmail.com>
2015-03-30 21:24:23 -0400

    Drafted Account layout.

--- app/src/main/java/com/niupiao/niupiao/fragments/account/AccountScreenFragment.java
+++ app/src/main/java/com/niupiao/niupiao/fragments/account/AccountScreenFragment.java
@@ -1,5 +1,6 @@
 package com.niupiao.niupiao.fragments.account;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -24,27 +25,11 @@ public class AccountScreenFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_myaccount, container, false);
 
-        User user = ((MainActivity) getActivity()).getUser();
-        TextView name = (TextView) root.findViewById(R.id.tv_name);
-        name.setText(user.getFirstName() + " " + user.getLastName());
+        Typeface black = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Black.ttf");
 
-        Button accountSettings = (Button) root.findViewById(R.id.btn_account_settings);
-        accountSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity().getApplicationContext(), "Account settings clicked", Toast.LENGTH_SHORT).show();
-                ((AccountNavFragment) getParentFragment()).changeScreen(AccountNavFragment.AccountScreen.ACCOUNT_SETTINGS);
-            }
-        });
+        TextView title = (TextView) root.findViewById(R.id.tv_title);
 
-        Button paymentSettings = (Button) root.findViewById(R.id.btn_payment_settings);
-        paymentSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity().getApplicationContext(), "Payment settings clicked", Toast.LENGTH_SHORT).show();
-                ((AccountNavFragment) getParentFragment()).changeScreen(AccountNavFragment.AccountScreen.PAYMENT_SETTINGS);
-            }
-        });
+        title.setTypeface(black);
 
         return root;
     }
